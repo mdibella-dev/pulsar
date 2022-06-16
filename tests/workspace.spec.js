6c305737112c659f6323548dc254d59146c98311
Maurício Szabo <mauricio.szabo@gmail.com>
2022-06-16 19:27:32 -0300

    Script to build APM

--- tests/workspace.spec.js
+++ tests/workspace.spec.js
@@ -5,7 +5,9 @@ const { test, expect } = require('@playwright/test');
 test('Checking that Atom opens at the welcome page', async () => {
   const env = process.env
   env.ATOM_HOME = "/tmp/atom-home-tests"
-  const electronApp = await electron.launch({
+  env.APM_PATH = "apm/node_modules/.bin/apm"
+
+  const config = {
     args: ["src/main-process/main.js"],
     cwd: "./",
     env: env,
@@ -13,7 +15,8 @@ test('Checking that Atom opens at the welcome page', async () => {
     //   dir: "/tmp/video.mp4"
     // },
     timeout: 10000
-  });
+  }
+  const electronApp = await electron.launch(config);
 
   const page = await electronApp.firstWindow();
 
